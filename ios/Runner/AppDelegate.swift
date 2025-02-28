f9cc591e1169b690ddff6a5cf4890ff479859562
Lenovo <samy.elmenshawy96@gmail.com>
2025-02-28 17:03:25 +0200

    New iOS folder changes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,7 +9,7 @@ import FirebaseCore
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDsEV_EHCaHHhEfI5_XxqeTa9gkXm5x_vU")
+    GMSServices.provideAPIKey("AIzaSyA5rARsu5IE9C448JTjRXuxMfkCbbXFV4w")
     FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
